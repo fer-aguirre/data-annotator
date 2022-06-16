b92c2bbcf4e5fc4c75f5c0f4d6df4bbe220d931e
Fer Aguirre <kay_op@protonmail.com>
2022-06-16 16:39:33 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Data Annotator',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Web application for data labeling',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
